cdce94b1fa2741f69f9afa04fa162d3c55248f0d
Henry Tran <h98.tran@gmail.com>
2020-10-18 23:21:39 -0400

    Day 1.4 - Homepage Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from './components/Header'
-import Safety from './components/pages/Safety';
+// import Safety from './components/pages/Safety';
+import Homepage from './components/pages/Homepage';
 import './default.scss';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Safety />
+      <Homepage />
     </div>
   );
 }
--- src/components/pages/Homepage/index.js
+++ src/components/pages/Homepage/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './styles.scss';
+import Shield from '../../../assets/shield-white-tablet-mobile-4.svg';
 
 const Homepage = props => {
     return (
-        <div>
-            <h1>Hello World!</h1>
+        <div className="flex-column">
+            <div className="flex">
+                <div className="container">
+                    <h1>Grillber with care</h1>
+                    <h3>Help keep each other safe</h3>
+                    <p className="black">The health and safety of the Grillber community is always our priority. We are actively monitoring the coronavirus (COVID‑19) situation and are taking steps to help keep those that rely on our platform safe.</p>
+                    <div className="button flex">
+                        <p>Learn more</p>
+                    </div>
+                </div>
+                <div>
+                    <img src={Shield} alt="Shield" />
+                </div>
+            </div>
         </div>
     );  
 };
