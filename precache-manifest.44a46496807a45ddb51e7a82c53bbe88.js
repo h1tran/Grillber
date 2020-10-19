d40d1243d5c3ae4d19cd6168b4f099cb45c9feb5
Henry Tran <h98.tran@gmail.com>
2020-10-19 11:28:42 -0400

    Updates

---
+++ precache-manifest.44a46496807a45ddb51e7a82c53bbe88.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c4c2c45b85979189e168bce7486448c",
+    "url": "/Grillber/index.html"
+  },
+  {
+    "revision": "dc79d627f23d589b0e94",
+    "url": "/Grillber/static/css/main.6b8eedfe.chunk.css"
+  },
+  {
+    "revision": "62ac6a0cc3104a487919",
+    "url": "/Grillber/static/js/2.701e0b6f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Grillber/static/js/2.701e0b6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc79d627f23d589b0e94",
+    "url": "/Grillber/static/js/main.2fd2ed4e.chunk.js"
+  },
+  {
+    "revision": "2e4355d9f1ae378dd9d8",
+    "url": "/Grillber/static/js/runtime-main.2cac7c0e.js"
+  },
+  {
+    "revision": "55170d1c9e41c752658980e5ea3f4283",
+    "url": "/Grillber/static/media/UberMoveMedium.55170d1c.otf"
+  },
+  {
+    "revision": "152f201134e28e5556e645ead9a76d6f",
+    "url": "/Grillber/static/media/UberMoveTextLight.152f2011.otf"
+  },
+  {
+    "revision": "56a881f549bee0679adf459053b532f1",
+    "url": "/Grillber/static/media/UberMoveTextRegular.56a881f5.otf"
+  },
+  {
+    "revision": "c6ef9c338c64208349121ac040787898",
+    "url": "/Grillber/static/media/mask-girl.c6ef9c33.jpg"
+  },
+  {
+    "revision": "dbfdbd995067486ee6fafa010405f5a6",
+    "url": "/Grillber/static/media/shield-white-tablet-mobile-4.dbfdbd99.svg"
+  }
+]);
