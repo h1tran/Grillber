5d9c854db4c3b996b8ad9ce334bf4e7fcc7aa24c
Henry Tran <h98.tran@gmail.com>
2020-10-29 16:48:04 -0400

    Day 5 - Company Dropdown Menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,6 +3,38 @@ import './styles.scss';
 import { Link } from 'react-router-dom';
 
 const Header = props => {
+
+    function showDropdownContent() {
+        document.getElementById("myDropdown").classList.toggle("show");
+        document.getElementById("bg-highlight").classList.toggle("bg-active");
+    }
+
+    // On click, if the element is NOT the button, then toggle "show" to remove dropdown.
+    window.onclick = function(event) {
+        if (!document.getElementById("bg-highlight").contains(event.target)) {
+            if (document.getElementById("bg-highlight").classList.contains("bg-active"))
+                document.getElementById("bg-highlight").classList.toggle("bg-active");
+            let dropdowns = document.getElementsByClassName("dropdown-content");
+            for (let i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+
+    function toggleArrow() {
+        if (document.getElementById("arrow-down").classList.contains("fa-angle-down")) {
+            document.getElementById("arrow-down").classList.remove("fa-angle-down");
+            document.getElementById("arrow-down").classList.add("fa-angle-up");
+        }
+        else {
+            document.getElementById("arrow-down").classList.remove("fa-angle-up");
+            document.getElementById("arrow-down").classList.add("fa-angle-down");
+        }
+    }
+
     return (
         <header className="header">
             <div className="wrap flex-2">
@@ -10,9 +42,24 @@ const Header = props => {
                     <Link to="/">
                         <p className="logo">Grillber</p>
                     </Link>
-                    <div className="hover flex">
-                        <p>Company</p>
-                        <i class="fas fa-angle-down white"></i>
+                    <div className="dropdown">
+                        <div id="bg-highlight" className="hover flex flexgap-4" onClick={() => {
+                            showDropdownContent();
+                            toggleArrow();
+                        }}>
+                            <p>Company</p>
+                            <i id="arrow-down" className="fas fa-angle-down fa-xs white"></i>
+                        </div> 
+                        <div id="myDropdown" className="transition-1s dropdown-content font">
+                            <p>About us</p>
+                            <p>Our offerings</p>
+                            <p>How Grillber works</p>
+                            <p>Global citizenship</p>
+                            <p>Newsroom</p>
+                            <p>Investor relations</p>
+                            <p>Blog</p>
+                            <p>Careers</p>
+                        </div>
                     </div>
                     <Link to='/Safety'>
                         <div className="hover flex">
@@ -28,15 +75,15 @@ const Header = props => {
                 </div>
                 <div className="flex">
                     <div className="hover flex">
-                        <i class="fas fa-globe white"></i>
+                        <i className="fas fa-globe white"></i>
                         <p>EN</p>
                     </div>
                     <div className="hover flex">
-                        <i class="far fa-circle white"></i>
+                        <i className="far fa-circle white"></i>
                         <p>Products</p>
                     </div>
                     <div className="hover flex">
-                        <i class="far fa-user white"></i>
+                        <i className="far fa-user white"></i>
                         <p>Log in</p>
                     </div>
                     <Link to="/User-Signup">
